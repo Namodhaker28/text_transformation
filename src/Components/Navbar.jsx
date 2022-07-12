76d0cad7dda59dc61c28fb574843f2d84d1a68ca
[namodhaker] <[namodhaker76@gmail.com]>
2022-07-12 16:25:34 +0530

    basic setup

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function () {
+  return (
+    
+       <nav className="navbar bg-dark navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand">Home</a>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+export default function Home () {
+    let [text,setText]=useState("")
+    
+
+    const OnChange=(e)=>{
+      setText(e.target.value)
+    }
+
+    const ToUpper=()=>
+    {
+      let newtext=text.toUpperCase()
+      setText(newtext)
+    }
+    const ToLower=()=>
+    {
+      let newtext=text.toLowerCase()
+      setText(newtext)
+    }
+    const ToClear=()=>
+    {
+      setText("")
+    }
+
+  return (
+    <div className='container mt-5' >
+      <h2>Enter your text here .....  </h2>
+        <div className="">
+        <textarea className="form-control " name="text"value={text} onChange={OnChange} cols="" rows="10"></textarea>
+        </div>
+        
+        <button className="btn btn-primary mt-3 mx-2"  onClick={ToUpper}>UPPERCASE</button>
+        <button className="btn btn-primary mt-3 mx-2" onClick={ToLower}>lowercase</button>
+        <button className="btn btn-primary mt-3 mx-2" onClick={ToClear}>Clear</button>
+        <div className="container mt-3">
+          <h2>SUMMARY</h2>
+          <h3>Total number of characters : {text.length}</h3>
+          <h3>Total number of words  : {text.split(" ").length}</h3>
+        </div>
+    </div>
+  )
+}
